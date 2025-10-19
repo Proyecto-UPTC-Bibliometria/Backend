557f97a015c2428fe7a959e37f8ff4824b44cd7a
Sebastián Lozano <sebastianl2.dev@gmail.com>
2025-10-19 12:09:01 -0500

    feat: Add a query builder to build search query for mongo

---
+++ src/utils/queryBuilder.ts
@@ -0,0 +1,41 @@
+import FilterConfig from "../interfaces/filters/filter.config.interface.js";
+import { FilterQuery } from "mongoose";
+
+export function buildQuery<T>(
+  filters: Record<string, any>,
+  config: Record<string, FilterConfig>
+): FilterQuery<T> {
+  const query: FilterQuery<T> = {};
+
+  Object.entries(filters).forEach(([key, value]) => {
+    if (value === undefined || value === null || value === "") return;
+
+    const filterConfig = config[key];
+    if (!filterConfig) return;
+
+    const fieldName = filterConfig.field || key;
+
+    switch (filterConfig.type) {
+      case "regex":
+        (query as any)[fieldName] = {
+          $regex: value.replaceAll("%20", " "),
+          $options: "i",
+        };
+        break;
+      case "exact":
+        (query as any)[fieldName] = {
+          $regex: value.replaceAll("%20", " "),
+          $options: "i",
+        };
+        break;
+      case "boolean":
+        (query as any)[fieldName] = value === true || value === "true";
+        break;
+      case "range":
+        // Separated range manager.
+        break;
+    }
+  });
+
+  return query;
+}
